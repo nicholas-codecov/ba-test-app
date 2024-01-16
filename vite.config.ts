0be4902cc431397b7ab583b811a274ca73351ad8
nicholas-codecov <nicholas.deschenes@sentry.io>
2024-01-16 14:37:35 -0400

    update config and deps

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { codecovVitePlugin } from "@codecov/vite-plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    codecovVitePlugin({
+      // Ensure this is set to true for bundle analysis
+      dryRun: true,
+      enableBundleAnalysis: true,
+      bundleName: "ba-test-app-bundle",
+      apiUrl: "http://localhost:8000",
+      uploadToken: "230d537c-d1ed-426f-afe8-61f62f73d015",
+    }),
+  ],
+  
+});
